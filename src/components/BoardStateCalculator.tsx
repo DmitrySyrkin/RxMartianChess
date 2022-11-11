e5666992448299746b22da052e86ab8210105b6e
Dmitry Syrkin <dmitry@syrkin.com>
2022-11-11 14:34:51 +0300

    chore: selection logic clean up

--- src/components/BoardStateCalculator.tsx
+++ src/components/BoardStateCalculator.tsx
@@ -1,6 +1,14 @@
 import { BoardProps } from "./Board";
 import { Figure } from "./Field";
 
+const belongsToCurrentPlayer = (clickedRow: number, isRedTurn: boolean) => {
+  return (clickedRow > 3 && isRedTurn) || (clickedRow < 4 && !isRedTurn);
+};
+
+const isValidMove = (activeFigure: Figure, figures: Figure[][]) => {
+  return true;
+};
+
 export const calculateState = (
   state: BoardProps,
   clickedRow: number,
@@ -10,27 +18,34 @@ export const calculateState = (
   let updatedSelected = state.selected;
   let updatedIsRedTurn = state.isRedTurn;
 
+  // select
   if (state.selected == null) {
     if (belongsToCurrentPlayer(clickedRow, state.isRedTurn)) {
       updatedSelected = { row: clickedRow, col: clickedCol };
     }
-  } else if (
-    updatedSelected.row == clickedRow &&
-    updatedSelected.col == clickedCol
-  ) {
-    updatedSelected = null;
-  }
-
-  if (
-    state.selected != null &&
-    belongsToCurrentPlayer(clickedRow, state.isRedTurn) &&
-    updatedFigures[clickedRow][clickedCol] == Figure.None
-  ) {
-    updatedFigures[clickedRow][clickedCol] =
-      updatedFigures[state.selected.row][state.selected.col];
-    updatedFigures[state.selected.row][state.selected.col] = Figure.None;
-    updatedSelected = null;
-    updatedIsRedTurn = !updatedIsRedTurn;
+  } else {
+    if (
+      updatedSelected.row == clickedRow &&
+      updatedSelected.col == clickedCol
+    ) {
+      updatedSelected = null;
+    } else if (
+      belongsToCurrentPlayer(clickedRow, state.isRedTurn) &&
+      updatedFigures[clickedRow][clickedCol] != Figure.None
+    ) {
+      updatedSelected = { row: clickedRow, col: clickedCol };
+    } else if (
+      updatedFigures[clickedRow][clickedCol] == Figure.None ||
+      (updatedFigures[clickedRow][clickedCol] != Figure.None &&
+        !belongsToCurrentPlayer(clickedRow, state.isRedTurn) &&
+        isValidMove(updatedFigures[state.selected.row][state.selected.col], updatedFigures))
+    ) {
+      updatedFigures[clickedRow][clickedCol] =
+        updatedFigures[state.selected.row][state.selected.col];
+      updatedFigures[state.selected.row][state.selected.col] = Figure.None;
+      updatedSelected = null;
+      updatedIsRedTurn = !updatedIsRedTurn;
+    }
   }
 
   return {
@@ -38,8 +53,4 @@ export const calculateState = (
     figures: updatedFigures,
     selected: updatedSelected,
   };
-};
-
-const belongsToCurrentPlayer = (clickedRow: number, isRedTurn: boolean) => {
-  return (clickedRow > 3 && isRedTurn) || (clickedRow < 4 && !isRedTurn);
 };
