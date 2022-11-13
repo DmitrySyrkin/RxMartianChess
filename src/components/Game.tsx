bec103ae31386e231959814c6e8f83d23071f202
Dmitry Syrkin <dmitry@syrkin.com>
2022-11-13 23:56:14 +0300

    chore: design updated, text info blocks added

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -24,12 +24,9 @@ export class Game extends React.Component {
   render() {
     return (
       <div className="game">
+        <h1>Martian Chess</h1>
         <div className="game-board">
           <Board {...startingBoard} />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
